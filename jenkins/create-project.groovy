a01081c8cca805352f89571f0a6cd6c515af7814
nlitterat <nlitterat@gmail.com>
2021-10-26 16:54:00 +0300

    Configured to my env

--- jenkins/create-project.groovy
+++ jenkins/create-project.groovy
@@ -1,20 +1,20 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://https://github.com/nlitterat/node-hello.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('nlitterat')
+            node / gitConfigEmail('nlitterat@gmail.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('Node 17.0.1') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('nlitterat/compie')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
